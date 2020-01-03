a4ea5c00a39865ca4f9b8c538fc5057b4c71d0b4
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-03 04:10:23 -0300

    Added Day15

---
+++ Day15/Solution.py
@@ -0,0 +1,11 @@
+#!/bin/python
+# compatible with python3
+
+def insert(self,head,data): 
+    if head is None:
+        head = Node(data)
+    elif head.next is None:
+        head.next = Node(data)
+    else: 
+        self.insert(head.next, data)
+    return head
